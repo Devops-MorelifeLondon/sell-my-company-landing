e3e1ec29fd7cff0568f4a9da17a3e2e40ee651c8
Devops-MorelifeLondon <devops@morelifelondon.net>
2025-10-25 15:47:26 +0530

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter, Open_Sans } from 'next/font/google'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 const open = Open_Sans({ subsets: ['latin'] })
@@ -20,7 +21,18 @@ export default function RootLayout({
       <head>
         <meta name="robots" content="noindex, nofollow" />
         <link rel="preload" as="document" href="https://form.jotform.com/252543224958462" />
-        
+
+        {/* Microsoft Clarity Script */}
+        <Script id="clarity" >
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "tvng1ynt64");
+          `}
+        </Script>
+
       </head>
       <body className={open.className}>{children}</body>
     </html>
