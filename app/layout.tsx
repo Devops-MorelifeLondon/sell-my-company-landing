b326ab3ba4732726dd2c3004bb6dfa0b3345158a
Devops-MorelifeLondon <devops@morelifelondon.net>
2025-10-25 15:25:07 +0530

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,32 +21,6 @@ export default function RootLayout({
         <meta name="robots" content="noindex, nofollow" />
         <link rel="preload" as="document" href="https://form.jotform.com/252543224958462" />
         
-        {/* Microsoft Clarity Script */}
-        <script
-          type="text/javascript"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(c,l,a,r,i,t,y){
-                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-              })(window, document, "clarity", "script", "tq2ks2kbrm");
-            `,
-          }}
-        />
-
-        {/* Google tag (gtag.js) */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-810525438"></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'AW-810525438');
-            `,
-          }}
-        />
       </head>
       <body className={open.className}>{children}</body>
     </html>
